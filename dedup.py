9a58060ce9b1fd9f96fb87b0c9e04082ec5e4362
Jans Rautenbach <git@moat.rauten.co.za>
2021-01-14 11:00:03 +0200

    Hash dumping

--- dedup.py
+++ dedup.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import hashlib
+import json
 
 # Consts
 BLOCKSIZE = 1024**2
@@ -19,16 +20,22 @@ def hash_file(thepath):
 
 
 class MediaItem:
-    def __init__(self, item, album):
-        self.item = item
-        self.album = album
+    def __init__(self, item_name, item_path, album_name, album_path):
+        self.item = {
+            'name': item_name,
+            'path': item_path
+        }
+        self.album = {
+            'name': album_name,
+            'path': album_path
+        }
 
 
 def hash_dir(album):
     for item in os.scandir(album.path):
         if not item.name.endswith('.json'):
             thehash = hash_file(item.path)
-            mediaobj = MediaItem(item, album)
+            mediaobj = MediaItem(item.name, item.path, album.name, album.path)
             if thehash in _hashtbl:
                 _hashtbl[thehash].append(mediaobj)
             else:
@@ -44,7 +51,7 @@ def rmdupes(dirpath):
                 # os.unlink(item)
                 json_equiv = item.path + '.json'
                 if os.path.exists(json_equiv):
-                    print(' and .json')
+                    print(' (and .json)')
                     # os.unlink(json_equiv)
                 else:
                     print()
@@ -64,16 +71,20 @@ def dedupe_albums(parentpath):
             print('Moving', item_mv.path, 'to', multialb_name, end='')
             # os.rename(item_mv.path, multialb_path+'/'+item_mv.name)
             if os.path.exists(item_mv.path+'.json'):
-                print(' and .json')
+                print(' (and .json)')
                 # os.rename(item_mv.path+'.json', multialb_path+'/'+item_mv.name+'.json')
             else:
                 print()
 
             # delete other items of the same hash
             for item in items[1:]:
-                os.unlink(item.item.path)
+                print('Deleting residual:', item.item.path, end='')
+                # os.unlink(item.item.path)
                 if os.path.exists(item.item.path+'.json'):
-                    os.unlink(item.item.path+'.json')
+                    print(' (and .json)')
+                    # os.unlink(item.item.path+'.json')
+                else:
+                    print()
 
 
 if __name__ == '__main__':
@@ -91,9 +102,18 @@ if __name__ == '__main__':
                 albums.append(item)
 
     print('=== Building hashtable ===')
-    for album in albums:
-        print('Hashing', album.name)
-        hash_dir(album)
+    if os.path.exists('dedup_hash.json'):
+        print('Loading hashes from dump')
+        with open('dedup_hash.json', 'r') as dump:
+            _hashtbl = json.load(dump)
+
+    else:
+        for album in albums:
+            print('Hashing', album.name)
+            hash_dir(album)
+        print('Dumping hashes')
+        with open('dedup_hash.json', 'w') as dump:
+            json.dump(_hashtbl, dump)
 
     print('=== Deleting dupes outside albums ===')
     for nonalb in nonalbums:
